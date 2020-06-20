21717bb62337ddd4f4ecca6d7d08ce837646f307
Michael Schlottke-Lakemper <michael@sloede.com>
2020-06-21 01:16:47 +0200

    add coeff_modulus (broken)

--- src/SEAL.jl
+++ src/SEAL.jl
@@ -4,8 +4,8 @@ const seal_library_path = "/home/mschlott/.pool/seal/3.5.4/lib/libsealc.so"
 
 export version_major, version_minor, version_patch, version
 export EncryptionParameters, SchemeType, none, bfv, ckks, get_poly_modulus_degree,
-       set_poly_modulus_degree!, set_coeff_modulus!
-export Modulus, coeff_modulus_create
+       set_poly_modulus_degree!, set_coeff_modulus!, coeff_modulus
+export Modulus, bit_count, value, coeff_modulus_create
 
 include("version.jl")
 include("encryptionparams.jl")
--- src/encryptionparams.jl
+++ src/encryptionparams.jl
@@ -35,11 +35,23 @@ function set_poly_modulus_degree!(enc_param, degree)
   return nothing
 end
 
-#=function set_coeff_modulus!(enc_param, coeff_modulus)=#
-#=  # FIXME: get  coeff_modulus length=#
-#=  ccall((:EncParams_SetCoeffModulus, seal_library_path), Clong,=#
-#=        (Ptr{Cvoid}, UInt64, Ptr{Ptr{Cvoid}}),=#
-#=        enc_param.handle, length(coeff_modulus), coeff_modulus.handle)=#
-#=  return nothing=#
-#=end=#
+function set_coeff_modulus!(enc_param, coeff_modulus)
+  coeff_modulus_ptrs = Ptr{Cvoid}[cm.handle for cm in coeff_modulus]
+  ccall((:EncParams_SetCoeffModulus, seal_library_path), Clong,
+        (Ptr{Cvoid}, UInt64, Ptr{Ptr{Cvoid}}),
+        enc_param.handle, length(coeff_modulus), coeff_modulus_ptrs)
+  return nothing
+end
+
+function coeff_modulus(enc_param)
+  length = Ref{UInt64}(0)
+  modulusptr = Ref{Ptr{Cvoid}}(0)
+  ccall((:EncParams_GetCoeffModulus, seal_library_path), Clong,
+        (Ptr{Cvoid}, Ref{UInt64}, Ref{Ptr{Cvoid}}),
+        enc_param.handle, length, modulusptr)
+  @show modulusptr
+  @show modulusptr[]
+  modulus = Modulus[Modulus(unsafe_load(modulusptr[], i)) for i in 1:length[]]
+  return modulus
+end
 
