7ea54d4a1b1f8c1fa77592d57f3a647f591eb0f3
Michael Schlottke-Lakemper <michael@sloede.com>
2020-06-20 22:51:08 +0200

    Add version information

--- src/SEAL.jl
+++ src/SEAL.jl
@@ -1,5 +1,9 @@
 module SEAL
 
-# Write your package code here.
+const seal_library_path = "/home/mschlott/.pool/seal/3.5.4/lib/libsealc.so"
+
+export version_major, version_minor, version_patch, version
+
+include("version.jl")
 
 end
---
+++ src/version.jl
@@ -0,0 +1,25 @@
+
+function version_major()
+  val = Ref{UInt8}(0)
+  ccall((:Version_Major, seal_library_path), Clong, (Ref{UInt8},), val)
+  return convert(Int, val[])
+end
+
+function version_minor()
+  val = Ref{UInt8}(0)
+  ccall((:Version_Minor, seal_library_path), Clong, (Ref{UInt8},), val)
+  return convert(Int, val[])
+end
+
+function version_patch()
+  val = Ref{UInt8}(0)
+  ccall((:Version_Patch, seal_library_path), Clong, (Ref{UInt8},), val)
+  return convert(Int, val[])
+end
+
+function version()
+  major = version_major()
+  minor = version_minor()
+  patch = version_patch()
+  return VersionNumber("$major.$minor.$patch")
+end
